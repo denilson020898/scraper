b6e06c4d84cd46b1f64d11a56b021b2990eda529
Denilson <denilson020898@gmail.com>
2020-11-26 15:38:39 +0700

    clean up

--- solution.js
+++ solution.js
@@ -1,56 +1,82 @@
+const Promise = require("bluebird");
 const cheerio = require('cheerio');
 const superagent = require('superagent');
-// const fs = require('fs');
-// const writeStream = fs.createWriteStream("solution.json");
+const fs = Promise.promisifyAll(require('fs'));
 
 const baseUrl = "https://www.cermati.com/artikel";
 let splitted = baseUrl.split("/");
 const prefix = splitted[0] + "//" + splitted[2];
 
-// var articles = { articles: [] };
-// writeStream.write(JSON.stringify(articles, null, "  "));
 
 superagent
     .get(baseUrl)
     .then(res => {
-        let myArr = [];
         const $ = cheerio.load(res.text);
+        let articleUrls = [];
 
         $(".list-of-articles").each((i, el) => {
             $(el).find(".article-list-item").each((i, el) => {
                 const url = (prefix + $(el).find("a").attr("href")).trim();
+                articleUrls.push(url);
+            })
+        })
 
-                superagent
-                    .get(url)
-                    .then(res => {
-                        const $ = cheerio.load(res.text);
-                        const title = $(".post-title").text().trim();
-                        const author = $(".author-name").text().trim();
-                        const postingDate = $(".post-date").text().trim();
+        return articleUrls;
+    })
+    .then(urls => {
+        const promises = urls.map(url => superagent.get(url));
+        return Promise.all(promises);
+    })
+    .then(responses => {
+        let jsonDataList = [];
+        for (const response of responses) {
 
-                        // const relatedArticlesList = $("")
+            const $ = cheerio.load(response.text);
+            const url = $(".OUTBRAIN").attr("data-src");
+            const title = $(".post-title").text().trim();
+            const author = $(".author-name").text().trim();
+            const postingDate = $(".post-date").text().trim();
 
-                        var json = {
-                            url,
-                            title,
-                            author,
-                            postingDate,
-                        }
-                        myArr.push(json);
-                        // writeStream.write(JSON.stringify(json, null, "        "));
-                        // writeStream.write(",\n");
+            var relatedArticles = [];
 
-                        // console.log(url, title, author, postingDate);
-                    });
+            $(".panel-header:contains('Artikel Terkait')").next().each((i, el) => {
+                $(el).find("li").each((i, el) => {
+                    const url = (prefix + $(el).find("a").attr("href").trim()).trim();
+                    const title = $(el).find(".item-title").text().trim()
 
+                    let relatedData = {
+                        url,
+                        title
+                    }
+                    relatedArticles.push(relatedData)
+                });
             });
-        });
 
-        return myArr;
+
+            let jsonData = {
+                url,
+                title,
+                author,
+                postingDate,
+                relatedArticles
+            };
+            jsonDataList.push(jsonData);
+        }
+        return jsonDataList
     })
     .then(values => {
-        console.log(values)
+        var scrapedData = {
+            articles: []
+        };
+        for (const value of values) {
+            scrapedData.articles.push(value)
+        }
+        return scrapedData;
+    })
+    .then(data => {
+        const filename = __filename.split(/[\\/]/).pop().split(".")[0];
+        fs.writeFileSync(filename + ".json", JSON.stringify(data, null, "  "));
     })
     .catch(err => {
-        console.log('scraping error: ', err);
+        console.log("scraping error: ", err);
     });
